f5e4681e7213c9d08e956468247bd14cf3c41694
Hashin Mohammed <hashinmohammed@HASHINs-MacBook-Air.local>
2023-05-31 22:13:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Home from './pages/Home';
+import Canvas from './canvas/index';
+import Customizer from './pages/Customizer';
+
+
+function App() {
+
+
+  return (
+<main className="app transition-all ease-in">
+  <Home />
+  <Canvas />
+  <Customizer />
+</main>
+  )
+}
+
+export default App
